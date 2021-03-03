c7878a0fd5bd54dd99d692e94d1329e93a56157b
Lio <00700mm@gmail.com>
2021-03-04 00:35:08 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+function index() {
+  return <IndexBlock>Index</IndexBlock>
+}
+
+const IndexBlock = styled.div`
+  text-align: center;
+`
+
+export default index
